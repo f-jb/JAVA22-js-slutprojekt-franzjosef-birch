59b815dcf81cf575f51b9c01b2b20013dde307f0
Franz - Josef Björck <fj.bjorck@protonmail.com>
2023-01-31 23:58:27 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,200 @@
+"use strict";
+
+class Model {
+  #page = 1;
+  #baseURL;
+  #key;
+  #url;
+
+  constructor() {
+    this.#baseURL = "https://api.flickr.com/services/rest/";
+    this.#key = "90dd63c6012cc6ab2e71337f54b9f7fe";
+  }
+  increasePage() {
+    this.#page++;
+  }
+  decreasePage() {
+    this.#page--;
+  }
+  resetPage() {
+    this.#page = 1;
+  }
+  getCurrentPage() {
+    return this.#page;
+  }
+  async getResults(searchQuery) {
+    this.#url = `${this.#baseURL}?method=flickr.photos.search&api_key=${
+      this.#key
+    }&text=${searchQuery.term}&per_page=${
+      searchQuery.perPage
+    }&page=${this.getCurrentPage()}&sort=${
+      searchQuery.sort
+    }&format=json&nojsoncallback=1`;
+    await fetch(this.#url, {
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((searchResults) => app.displayResults(searchResults.photos))
+      .catch((error) => app.view.setStatus(error.message));
+    return this.searchResults;
+  }
+}
+
+class View {
+  #resultView;
+  #statusView;
+  #nav;
+  #searchTerms;
+  #perPage;
+  #sort;
+  #nextButton;
+  #previousButton;
+  #searchButton;
+
+  constructor() {
+    this.#searchButton = document.getElementById("search");
+    this.#previousButton = document.getElementById("previous");
+    this.#nextButton = document.getElementById("next");
+    this.#resultView = document.getElementById("resultView");
+    this.#statusView = document.getElementById("statusView");
+    this.#nav = document.getElementById("nav");
+    this.#searchTerms = document.getElementById("searchQuery");
+    this.#perPage = document.getElementById("perPage");
+    this.#sort = document.getElementById("sort");
+  }
+  getSearchButton() {
+    return this.#searchButton;
+  }
+  getNextButtion() {
+    return this.#nextButton;
+  }
+  getPreviousButton() {
+    return this.#previousButton;
+  }
+  enableNextButton() {
+    this.#nextButton.disabled = false;
+  }
+  disableNextButton() {
+    this.#nextButton.disabled = true;
+  }
+  enablePreviousButton() {
+    this.#previousButton.disabled = false;
+  }
+  disablePreviousButton() {
+    this.#previousButton.disabled = true;
+  }
+  enableNav() {
+    this.#nav.style.display = "block";
+  }
+  disableNav() {
+    this.#nav.style.display = "none";
+  }
+  getSearchQuery() {
+    const searchQuery = {
+      term: this.#searchTerms.value,
+      perPage: this.#perPage.value,
+      sort: this.#sort.value,
+    };
+    return searchQuery;
+  }
+
+  setStatus(message) {
+    this.messageText = document.createElement("p");
+    this.messageText.innerText = message;
+    this.#statusView.replaceChildren(this.messageText);
+  }
+  cleanResultsView() {
+    while (this.#resultView.firstChild) {
+      this.#resultView.removeChild(this.#resultView.firstChild);
+    }
+  }
+  getResultView() {
+    return this.#resultView;
+  }
+  displayResults(photos) {
+    this.cleanResultsView();
+
+    for (
+      let i = 0;
+      i < (photos.perpage > photos.total ? photos.total : photos.perpage);
+      i++
+    ) {
+      const img = document.createElement("img");
+      const photo = photos.photo[i];
+      const photoSize = document.getElementById("size").value;
+      img.src = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}${photoSize}.jpg`;
+
+      this.#resultView.appendChild(img);
+    }
+  }
+}
+
+class Controller {
+  model;
+  view;
+
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+    view.getSearchButton().addEventListener("click", this.search);
+    view.getNextButtion().addEventListener("click", this.nextPage);
+    view.getPreviousButton().addEventListener("click", this.previousPage);
+    view.getResultView().addEventListener("click", (e) => {
+      if (e.target.nodeName === "IMG") {
+        open(e.target.src);
+      }
+    });
+  }
+
+  displayResults(searchResults) {
+    if (searchResults.total === 0) {
+      app.view.cleanResultsView();
+      app.view.setStatus("No results found");
+    } else {
+      app.view.displayResults(searchResults);
+      app.view.setStatus(
+        `Page ${app.model.getCurrentPage()} of ${searchResults.pages}`
+      );
+      if (searchResults.total > searchResults.perpage) {
+        app.view.enableNav();
+        if (searchResults.page === 1) {
+          app.view.disablePreviousButton();
+        } else {
+          app.view.enablePreviousButton();
+        }
+        if (searchResults.page === searchResults.pages) {
+          app.view.disableNextButton();
+        } else {
+          app.view.enableNextButton();
+        }
+      } else {
+        app.view.disableNav();
+      }
+    }
+  }
+
+  search(e) {
+    if (e.target.id === "search") {
+      app.model.resetPage();
+    }
+    e.preventDefault();
+    const searchQuery = app.view.getSearchQuery();
+    if (searchQuery.term === "") {
+      app.view.setStatus("please enter a search term");
+    } else {
+      app.model.getResults(searchQuery);
+    }
+  }
+  nextPage(e) {
+    e.preventDefault();
+    app.model.increasePage();
+    app.search(e);
+  }
+  previousPage(e) {
+    e.preventDefault();
+    app.model.decreasePage();
+    app.search(e);
+  }
+}
+
+const app = new Controller(new Model(), new View());
---
+++ js/ts_script.js
@@ -0,0 +1,119 @@
+"use strict";
+const baseURL = "https://api.flickr.com/services/rest/";
+const key = "90dd63c6012cc6ab2e71337f54b9f7fe";
+const searchButton = document.getElementById("search");
+const resultView = document.getElementById("resultView");
+const statusView = document.getElementById("statusView");
+const nav = document.getElementById("nav");
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+let page = 0;
+searchButton === null || searchButton === void 0
+  ? void 0
+  : searchButton.addEventListener("click", fetchResults);
+previousButton === null || previousButton === void 0
+  ? void 0
+  : previousButton.addEventListener("click", fetchResults);
+nextButton === null || nextButton === void 0
+  ? void 0
+  : nextButton.addEventListener("click", fetchResults);
+resultView === null || resultView === void 0
+  ? void 0
+  : resultView.addEventListener("click", (e) => {
+      if (e.target.nodeName === "IMG") {
+        open(e.target.src);
+      }
+    });
+function fetchResults(e) {
+  e.preventDefault();
+  if (e.target === searchButton) {
+    page = 1;
+  }
+  if (e.target === nextButton) {
+    page++;
+  }
+  if (e.target === previousButton) {
+    page--;
+  }
+  const searchQuery = document.getElementById("searchQuery").value;
+  const perPage = document.getElementById("perPage").value;
+  const sort = document.getElementById("sort").value;
+  if (searchQuery != "") {
+    const url = `${baseURL}?method=flickr.photos.search&api_key=${key}&text=${searchQuery}&per_page=${perPage}&page=${page}&sort=${sort}&format=json&nojsoncallback=1`;
+    fetch(url, {
+      mode: "cors",
+    })
+      .then((response) => response.json())
+      .then((json) => displayResults(json))
+      .catch((error) => setStatus(error.message));
+  } else {
+    while (
+      resultView === null || resultView === void 0
+        ? void 0
+        : resultView.firstChild
+    ) {
+      resultView.removeChild(resultView.firstChild);
+    }
+    setStatus("Please enter a search term.");
+  }
+}
+function setStatus(error) {
+  const errorText = document.createElement("p");
+  errorText.innerText = error;
+  statusView === null || statusView === void 0
+    ? void 0
+    : statusView.replaceChildren(errorText);
+}
+function displayResults(json) {
+  while (
+    resultView === null || resultView === void 0
+      ? void 0
+      : resultView.firstChild
+  ) {
+    resultView.removeChild(resultView.firstChild);
+  }
+  if (json.photos.total === 0) {
+    while (
+      resultView === null || resultView === void 0
+        ? void 0
+        : resultView.firstChild
+    ) {
+      resultView.removeChild(resultView.firstChild);
+    }
+    setStatus("No images found.");
+  } else {
+    statusView.innerText = `Page ${page} of ${Math.ceil(
+      json.photos.total / json.photos.perpage
+    )}`;
+    for (
+      let i = 0;
+      i <
+      (json.photos.perpage > json.photos.total
+        ? json.photos.total
+        : json.photos.perpage);
+      i++
+    ) {
+      const img = document.createElement("img");
+      const photo = json.photos.photo[i];
+      const photoSize = document.getElementById("size").value;
+      img.src = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}${photoSize}.jpg`;
+      resultView.appendChild(img);
+    }
+    if (json.photos.total > json.photos.perpage) {
+      nav.style.display = "block";
+      if (json.photos.page === 1) {
+        previousButton.disabled = true;
+      } else {
+        previousButton.disabled = false;
+      }
+      if (json.photos.perpage * json.photos.page >= json.photos.total) {
+        nextButton.disabled = true;
+      } else {
+        nextButton.disabled = false;
+      }
+    } else {
+      nav.style.display = "none";
+    }
+  }
+}
+//# sourceMappingURL=script.js.map
